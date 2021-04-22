3bdc61f705324a398eb83bd0fadb15afbb8f3e7f
TDLorenz <troydaniello.nyc@gmail.com>
2021-04-22 19:49:36 -0400

    used HashRouter insetad of browseroruter in order to deploy to gh pages

--- zipcode-city-search/src/App.js
+++ zipcode-city-search/src/App.js
@@ -1,24 +1,25 @@
 import React, { Component, useState } from 'react';
 import './App.css';
 import ZipSearch from './components/ZipSearch';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+//import {BrowserRouter as Router, Route, Swith} from 'react-router-dom';
+import { HashRouter as Router, Route, Swith } from 'react-router-dom';
 import NavBar from "./components/NavBar"
 import CitySearch from './components/CitySearch';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  render(){
-  return (
-    <>
-    <Router>
-    <NavBar/>
-      
-        <Route exact path='/' component={ZipSearch}/>
-        <Route exact path='/CitySearch' component={CitySearch}/>
-      
-      </Router>
+  render() {
+    return (
+      <>
+        <Router>
+          <NavBar />
+
+          <Route exact path='/' component={ZipSearch} />
+          <Route exact path='/CitySearch' component={CitySearch} />
+
+        </Router>
       </>
-  );
+    );
   }
 }
 
