47f8326fa671973231314635a08d2fb1a8f6d7da
TDLorenz <troydaniello.nyc@gmail.com>
2021-04-22 21:31:13 -0400

    changed hashrouter back to browser router in app.js

--- zipcode-city-search/src/App.js
+++ zipcode-city-search/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component, useState } from 'react';
 import './App.css';
 import ZipSearch from './components/ZipSearch';
-//import {BrowserRouter as Router, Route, Swith} from 'react-router-dom';
-import { HashRouter as Router, Route, Swith } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Swith } from 'react-router-dom';
+//import { HashRouter as Router, Route, Swith } from 'react-router-dom';
 import NavBar from "./components/NavBar"
 import CitySearch from './components/CitySearch';
 import 'bootstrap/dist/css/bootstrap.min.css';
